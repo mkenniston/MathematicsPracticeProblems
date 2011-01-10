b05b6279f86848420683c1acbacf3f04f36b8f9c
Michael Kenniston <github@michaelkenniston.com>
2011-01-09 16:31:12 -0800

    Tweak upload script.

--- upload.bash
+++ upload.bash
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-scp -r problems msk@typhoon.xnet.com:public_html/domains/mathematicspracticeproblems.com/
+scp -r index.html problems msk@typhoon.xnet.com:public_html/domains/mathematicspracticeproblems.com/
+
+SCRIPT=upload-script.$$
+cat <<- ! >$SCRIPT
+ls public_html/domains
+cd public_html/domains/mathematicspracticeproblems.com
+chmod go+x problems
+chmod go+r index.html problems/*
+ls -l *
+exit 0
+!
+
+ssh msk@typhoon.xnet.com < $SCRIPT
+rm $SCRIPT
 
